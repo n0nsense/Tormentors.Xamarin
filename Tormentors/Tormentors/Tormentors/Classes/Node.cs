a78f875ce1c13b3bd9e29bd11dcd7036e1c7a2e1
n0nsense <jnonishe@yandex.ru>
2021-10-14 15:37:17 +0300

    Added list classes

---
+++ Tormentors/Tormentors/Tormentors/Classes/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tormentors.Classes
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+        public Node(T data)
+        {
+            Data = data;
+        }
+    }
+}
